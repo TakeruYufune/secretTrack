ee90f94b40cfefe6b2fadd79232744a9185b8ea3
TakeruYufune <27349791+TakeruYufune@users.noreply.github.com>
2020-08-28 22:46:16 +0900

    Initial Commit

---
+++ SecretTrack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecretTrack
+//
+//  Created by Takeru Yufune on 2020/08/28.
+//  Copyright © 2020 Takeru Yufune. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
